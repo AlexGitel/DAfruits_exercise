66ff327bdfecf2539ace3e04305d7c97daf20d44
Alex Gitel <alexeygitel@gmail.com>
2025-06-22 14:54:13 +0200

    started

---
+++ fruitbasket/src/app/shared/components/header/header.component.ts
@@ -0,0 +1,20 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [],
+  template: `
+  <header>   
+  <div>
+      <img src="./../../assets/img/31800109_lop2.svg" alt="" srcset="">
+      <p>DA</p>
+      <p>fruits</p>
+  </div>
+</header>`,
+
+  styleUrl: './header.component.scss'
+})
+export class HeaderComponent {
+
+}
